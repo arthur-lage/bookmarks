15e825e42f1e45b9a64e048ef5858c147553032c
Arthur Lage <arthurlage2006@gmail.com>
2022-10-18 19:36:08 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Bookmarks
+
+## ✉ About
+
+A project which you can save your favorite websites with descriptions.
+
+## 🌆 Images
+
+
+
+## ⚠ Requirements
+
+In order to run the project, you will need:
+
+- Node
+- A web browser (Chrome, Firefox, Edge, etc...)
+
+## 💻 Technologies Used
+
+### FRONTEND
+
+- HTML
+- CSS
+- JavaScript
+
+### BACKEND
+
+- Node.js
+- NestJS
+- Typescript
+- JsonWebToken
+- Prisma
+- PostgreSQL
+
+### EDITOR
+
+- Visual Studio Code
+
+## ⬇ How to download the project
+
+```bash
+
+git clone https://github.com/arthur-lage/bookmarks.git
+
+cd bookmarks
+
+# Server
+
+cd server 
+
+npm install
+
+npm run dev
+
+```
+
+## 🔗 How to use the app
+
+If you wish to test the application, click the link below:
+
+[app](https://link-to-your-app.com)
---
+++ server/src/auth/auth.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDTO } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { ForbiddenException } from '@nestjs/common/exceptions';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+
+  async signup(dto: AuthDTO) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      return this.signToken(user.id, user.email);
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002') {
+          throw new ForbiddenException('This email has already been taken.');
+        }
+      }
+    }
+  }
+
+  async signin(dto: AuthDTO) {
+    const userExists = await this.prisma.user.findFirst({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!userExists) throw new ForbiddenException('Invalid email or password.');
+
+    const isPasswordCorrect = await argon.verify(userExists.hash, dto.password);
+
+    if (!isPasswordCorrect)
+      throw new ForbiddenException('Invalid email or password.');
+
+    return this.signToken(userExists.id, userExists.email);
+  }
+
+  async signToken(
+    userId: string,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '1h',
+      secret: this.config.get('JWT_SECRET'),
+    });
+
+    return {
+      access_token: token,
+    };
+  }
+}
